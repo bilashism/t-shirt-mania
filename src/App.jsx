cb06765316b8f89bdb55160fa891b14450d0c929
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-02 12:47:53 +0600

    added a placeholder Banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Banner from "./components/Banner";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <figure>
-          <picture>
-            <source srcSet="../public/setting-up-dev-environment-react-vite-tailwind.webp" />
-            <img
-              src="../public/setting-up-dev-environment-react-vite-tailwind.webp"
-              width="730"
-              height="487"
-              loading="lazy"
-              decoding="async"
-              fetchpriority="low"
-              className="mx-auto"
-              alt="Setting Up Dev Environment with React, Vite, Tailwind"
-            />
-          </picture>
-        </figure>
+        <Banner></Banner>
         <p className="text-center">
           <button onClick={() => setCount(count => count + 1)}>
             count is {count}
