64b73574d902774d0ada232f0d973effe9e2e4ff
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-08 12:10:07 +0600

    added transition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,27 +3,22 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const activeClass = "text-red-600";
+  const setActiveClass = isActive => (isActive ? activeClass : "");
   return (
     <nav className="container mx-auto flex flex-wrap justify-center items-center gap-4 text-xl uppercase">
       <NavLink
         to="/home"
-        className={({ isActive }) =>
-          (isActive ? activeClass : undefined) + " transition "
-        }>
+        className={({ isActive }) => setActiveClass(isActive) + " transition "}>
         home
       </NavLink>
       <NavLink
         to="/orders"
-        className={({ isActive }) =>
-          (isActive ? activeClass : undefined) + " transition "
-        }>
+        className={({ isActive }) => setActiveClass(isActive) + " transition "}>
         orders
       </NavLink>
       <NavLink
         to="/about"
-        className={({ isActive }) =>
-          (isActive ? activeClass : undefined) + " transition "
-        }>
+        className={({ isActive }) => setActiveClass(isActive) + " transition "}>
         about
       </NavLink>
     </nav>
